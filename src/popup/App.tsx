e1ec618800c5140ba6a82716de87ab9b3c265681
George Berezhnoy <gohabereg@users.noreply.github.com>
2018-12-15 23:18:52 +0300

    Add redux and thunk (#2)

--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -1,33 +1,51 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
+import { Provider } from 'react-redux'
 import DashboardScreen from './screens/Dashboard'
 import LoginScreen from './screens/Login'
 import feathersClient from './FeathersClient'
 import styles from './App.css'
+import store from './redux/store'
+import { set as setUser, clear as clearUser } from './redux/reducers/user'
 
 class App extends Component {
   state = {
     user: feathersClient.user
   }
 
+  updateUser = (user) => {
+    if (user !== null) {
+      const { user: oldUser } = store.getState()
+
+      store.dispatch(setUser(user))
+    } else {
+      store.dispatch(clearUser())
+    }
+  }
+
   componentDidMount () {
     feathersClient.authenticate()
 
-    feathersClient.on('user', user => {
+    feathersClient.on('user', this.updateUser)
+    store.subscribe(() => {
+      const { user } = store.getState()
+
       this.setState({ user })
     })
   }
 
   render () {
     return (
-      <Router>
-        <main className={styles.app}>
-          <Route path='/' exact component={DashboardScreen} />
-          <Route path='/login' component={LoginScreen} />
+      <Provider store={store}>
+        <Router>
+          <main className={styles.app}>
+            <Route path='/' exact component={DashboardScreen} />
+            <Route path='/login' component={LoginScreen} />
 
-          <Redirect to={this.state.user ? '/' : '/login'} />
-        </main>
-      </Router>
+            <Redirect to={this.state.user ? '/' : '/login'} />
+          </main>
+        </Router>
+      </Provider>
     )
   }
 }
